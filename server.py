33f86282807e2c8a09f314a91d495a1263b9ceaf
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2018-03-27 23:42:06 +0300

    Add workaround to database migrations.

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ from youtube_queue.app import app
 from youtube_queue.models import Base, engine
 
 if __name__ == "__main__":
-    if "migrate" in sys.argv:
+    if "reset" in sys.argv:
+        os.remove("database.db")
         Base.metadata.create_all(engine)
     else:
         current_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
