78f0b78c1c2a3eaf5cff3d533caf25e37f8c884f
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2018-03-27 23:43:36 +0300

    Add tables for songs and assiciations.

--- youtube_queue/models.py
+++ youtube_queue/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import database_exists, create_database
@@ -13,16 +13,19 @@ if not database_exists(engine.url):
 
 
 class User(Base):
+    """
+    Single user can have many rooms which
+    again can have many users (admins)
+    """
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, primary_key=True)
     password = Column(String)
     # admin property can be accessed via room class
-    rooms = relationship("User", backref="admin")
+    rooms = relationship("AdminAssociation")
 
     def __repr__(self):
-        return "<User(id='{}', name='{}', password='{}')>".format(self.id, self.name, self.password)
+        return "<User(name='{}', password='{}')>".format(self.name, self.password)
 
 
 class Room(Base):
@@ -30,8 +33,32 @@ class Room(Base):
 
     name = Column(String, primary_key=True)
     password = Column(String)
+    queue = relationship("SongAssociation")
 
     def __repr__(self):
         return "<Room(name='{}', password='{}')>".format(self.name, self.password)
 
-Base.metadata.create_all(engine)
+
+class Song(Base):
+    """
+    A song can be played in many rooms
+    and a room can have many songs in queue.
+    """
+    __tablename__ = "songs"
+
+    song_id = Column(String, primary_key=True)
+    name = Column(String)
+
+
+class SongAssociation(Base):
+    __tablename__ = "songassociation"
+
+    song_id = Column(String, ForeignKey("songs.song_id"), primary_key=True)
+    name = Column(String, ForeignKey("rooms.name"), primary_key=True)  # Room name
+
+
+class AdminAssociation(Base):
+    __tablename__ = "adminassociation"
+    user_name = Column(String, ForeignKey('users.name'), primary_key=True)
+    room_name = Column(String, ForeignKey('rooms.name'), primary_key=True)
+    room = relationship("Room")
